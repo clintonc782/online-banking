388b78005bdddb008a5001d940e8ef3f71ad008b
clintonc782 <clintonnwachukwu521@gmail.com>
2026-04-06 10:51:53 +0100

    database

--- onlineBanking/settings.py
+++ onlineBanking/settings.py
@@ -85,12 +85,29 @@ WSGI_APPLICATION = 'onlineBanking.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# Determine the environment
+ENVIRONMENT = config('DJANGO_ENV', default='development')
+
+if ENVIRONMENT == 'production':
+    DATABASE_URL = os.getenv('DATABASE_URL')
+    if DATABASE_URL:
+        DATABASES = {
+            'default': dj_database_url.config(
+                default=DATABASE_URL,
+                conn_max_age=3600,
+                ssl_require=True
+            )
+        }
+    else:
+        raise ValueError("DATABASE_URL environment variable is not set in production.")
+else:  # Development environment
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
 
 
 # Password validation
